80f9797eb18f619eb11413d9eed11874388c3f27
xiaowang <1206414543@qq.com>
2019-04-19 19:34:56 +0800

    api接口完成

--- handler/main.py
+++ handler/main.py
@@ -1,10 +1,70 @@
+import os
 import tornado.web
+import tornado.gen
+import tornado.httpclient
 
 
 class DownLoad(tornado.web.RequestHandler):
 
     def get(self):
-        self.write('hello world')
+        self.render('download.html')
 
     def post(self):
-        pass
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # 拼接出直接路径
+        file = self.get_argument('filename', '')
+        self.set_header('Content-Type', 'application/octet-stream')
+        self.set_header('Content-Disposition', 'attachment; filename={}'.format(file))
+        path = os.path.join(BASE_DIR, 'data', file)
+
+        def down(size=512):
+            """
+            下载器
+            :param size:
+            :return:
+            """
+            with open(path, 'rb') as f:
+                while True:
+                    result = f.read(size)
+                    if result:
+                        self.write(result)
+
+                    else:
+                        break
+
+        down()
+        self.finish()
+
+
+
+
+
+
+
+
+'''
+def post():
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # 拼接出直接路径
+    file = self.get_argument('filename', '')
+    self.set_header('Content-Type', 'application/octet-stream')
+    self.set_header('Content-Disposition', 'attachment; filename={}'.format(file))
+    path = os.path.join(BASE_DIR, 'data', file)
+
+    def down(size=512):
+        """
+        下载器
+        :param size:
+        :return:
+        """
+        with open(path, 'rb') as f:
+            while True:
+                result = f.read(size)
+                if result:
+                    self.write(result)
+
+                else:
+                    break
+
+    down()
+    self.finish()
+'''
+
